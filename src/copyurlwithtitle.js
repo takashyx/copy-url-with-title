f4329240807b63b54be9d04522b93efb7de810b1
takashyx <takashyx@gmail.com>
2022-06-30 22:38:31 +0900

    Workaround for non-http pages

--- src/copyurlwithtitle.js
+++ src/copyurlwithtitle.js
@@ -73,8 +73,29 @@ function copyURLWithTitle(event) {
             console.log("toast options: " + JSON.stringify(options_for_toast));
         }
         if (override_copy) {
-            navigator.clipboard.writeText(text_for_clipboard).then(() => {
-            }).catch((error) => { alert(`Copy failed! ${error}`) });
+            if (navigator.clipboard && window.isSecureContext) {
+                // navigator clipboard api method'
+                console.log("Copy for https page ( navigator.clipboard.writeText()')");
+                navigator.clipboard.writeText(text_for_clipboard).then(() => {
+                }).catch((error) => { alert(`Copy failed! ${error}`) });
+            } else {
+                // text area method
+                console.log("Copy for non-https page ( document.execCommnad('copy')");
+                let textArea = document.createElement("textarea");
+                textArea.value = text_for_clipboard;
+                // make the textarea out of viewport
+                textArea.style.position = "fixed";
+                textArea.style.left = "-999999px";
+                textArea.style.top = "-999999px";
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+                return new Promise((res, rej) => {
+                    // here the magic happens
+                    document.execCommand('copy') ? res() : rej();
+                    textArea.remove();
+                });
+            }
         }
     });
 }
